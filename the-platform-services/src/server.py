defa25cffb6d2c00b17c4dba8943e6d36394b7dd
vivek <vivek.bnb@gmail.com>
2018-10-22 22:30:25 +0530

    Swagger path Updated

--- the-platform-services/src/server.py
+++ the-platform-services/src/server.py
@@ -7,14 +7,14 @@ from com.platform.core.web.home import HomeController
 from com.platform.health.diabetes.web.diabetes import DiabetesController, DiabetesDataSetController, DiabetesDataTestController, RegistrationController
 
 app = Flask(__name__)
-blueprint=Blueprint('api',__name__,url_prefix='/api')
 CORS(app)
+
+blueprint=Blueprint('api',__name__)
 api = Api(blueprint , catch_all_404s=True,doc='/documentation')
 app.register_blueprint(blueprint)
-
 #api = Api(app,api_version='0.0', api_spec_url='/api/swagger')
-
 app.config['SWAGGER_UI_JSONEDITOR']=True
+
 api.add_resource(HomeController, '/', endpoint="home")
 api.add_resource(DiabetesController, '/api/diabetes/<model_name>', endpoint="diabetes")
 api.add_resource(DiabetesDataSetController, '/api/diabetes/dataset/<name>', endpoint="diabetes/dataset")
